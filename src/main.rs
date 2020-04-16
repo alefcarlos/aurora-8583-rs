651887cce91832fc16d945428c7955bdfeeca748
Alef Carlos <alef.carlos@gmail.com>
2020-04-16 02:26:27 -0300

    feat: request parser

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
 
     let fields = vec![
         Field {
-            id: "0".to_string(),
+            id: "1".to_string(),
             value: "0100".to_string(),
         },
         Field {
@@ -18,19 +18,13 @@ fn main() {
     ];
 
     let request = ISORequestMessage { fields };
-    println!("Total: {}", request.fields.capacity());
 
-    println!("requrest is valid? {}", request.validate());
-
-    let mti = request.get_info("0".to_string());
-
-    match mti {
-        None => println!("MTI was not provided"),
-        Some(x) => println!("mti: {}", x),
+    if !request.is_valid() {
+        panic!("Required DE were not provided");
     }
 
-    let iso = ISOMessage::from(request);
-    println!("enum mti {}", iso.mti);
+    let iso = ISOMessage::from(&request);
+    println!("Happy end!");
 }
 
 struct ISORequestMessage {
@@ -48,23 +42,36 @@ impl ISORequestMessage {
         };
     }
 
+    ///Gets value from DE
+    fn get_evaluated_info(&self, id: String) -> String {
+        let value = self.get_info(id);
+        return value.unwrap_or_default();
+    }
+
     ///Validates if all the required DE were provided
-    fn validate(&self) -> bool {
+    fn is_valid(&self) -> bool {
         //without mti we cant perform validation
-        if !self.validate_mti() {
+        if !self.has_valid_mti() {
             return false;
         }
 
         //Get required de info
-
+        //TODO
         return true;
     }
 
     //Validates if MTI was informed
-    fn validate_mti(&self) -> bool {
-        return match self.get_info("0".to_string()) {
+    fn has_valid_mti(&self) -> bool {
+        return match self.get_mti() {
             Some(_) => true,
             _ => false,
+        };
+    }
+
+    fn get_mti(&self) -> Option<String> {
+        return match self.get_info("0".to_string()) {
+            Some(x) => Some(x),
+            _ => None,
         };
     }
 }
@@ -73,8 +80,7 @@ struct Field {
     id: String,
     value: String,
 }
-#[derive(PartialEq)]
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 enum MessageTypeIndicator {
     AuthorizationRequest,
     ReversalRequest,
@@ -89,6 +95,25 @@ impl fmt::Display for MessageTypeIndicator {
     }
 }
 
+impl MessageTypeIndicator {
+    fn from(request: &ISORequestMessage) -> Option<MessageTypeIndicator> {
+        let mti = request.get_mti();
+
+        if mti.is_none() {
+            return None;
+        }
+
+        //QUE BOXTA: https://stackoverflow.com/questions/48034119/how-can-i-pattern-match-against-an-optionstring
+        let mti = mti.as_ref().map(String::as_str);
+
+        return match mti {
+            Some("0100") => Some(MessageTypeIndicator::AuthorizationRequest),
+            Some("0400") => Some(MessageTypeIndicator::ReversalRequest),
+            _ => None,
+        };
+    }
+}
+
 ///An struct to represent ISO 8583-1
 struct ISOMessage {
     mti: MessageTypeIndicator,
@@ -96,11 +121,20 @@ struct ISOMessage {
 }
 
 impl ISOMessage {
-    fn from(request: ISORequestMessage) -> ISOMessage {
-        return ISOMessage {
-            mti: MessageTypeIndicator::AuthorizationRequest,
-            card_number: "5276600404324025".to_string(),
-        };
+    fn from(request: &ISORequestMessage) -> Option<ISOMessage> {
+        if !request.is_valid() {
+            return None;
+        }
+
+        let mti = MessageTypeIndicator::from(&request);
+        if mti.is_none() {
+            return None;
+        }
+
+        return Some(ISOMessage {
+            mti: mti.unwrap(),
+            card_number: request.get_evaluated_info("2".to_string()),
+        });
     }
 }
 
@@ -123,7 +157,7 @@ mod tests {
 
         let request = ISORequestMessage { fields };
 
-        assert_eq!(request.validate(), true);
+        assert_eq!(request.is_valid(), true);
     }
 
     #[test]
@@ -135,7 +169,7 @@ mod tests {
 
         let request = ISORequestMessage { fields };
 
-        assert_eq!(request.validate(), false);
+        assert_eq!(request.is_valid(), false);
     }
 
     #[test]
@@ -153,12 +187,31 @@ mod tests {
 
         let request = ISORequestMessage { fields };
 
-        assert_eq!(request.validate_mti(), true);
+        assert_eq!(request.has_valid_mti(), true);
         assert_eq!(request.get_info("0".to_string()), Some("0100".to_string()));
     }
 
     #[test]
     fn test_request_should_has_invalid_mti() {
+        let fields = vec![
+            Field {
+                id: "1".to_string(),
+                value: "0100".to_string(),
+            },
+            Field {
+                id: "2".to_string(),
+                value: "5276600404324025".to_string(),
+            },
+        ];
+
+        let request = ISORequestMessage { fields };
+
+        assert_eq!(request.has_valid_mti(), false);
+        assert_eq!(request.get_info("0".to_string()), None);
+    }
+
+    #[test]
+    fn test_parse_request_should_be_success() {
         let fields = vec![
             Field {
                 id: "0".to_string(),
@@ -172,21 +225,41 @@ mod tests {
 
         let request = ISORequestMessage { fields };
 
-        assert_eq!(request.validate_mti(), false);
-        assert_eq!(request.get_info("0".to_string()), None);
+        let iso = ISOMessage::from(&request);
+        assert_eq!(iso.is_some(), true);
+
+        let unwrap_iso = iso.unwrap();
+        assert_eq!(unwrap_iso.mti, MessageTypeIndicator::AuthorizationRequest);
+        assert_eq!(unwrap_iso.card_number, "5276600404324025");
     }
 
     #[test]
-    fn test_parse_request_should_be_success() {
+    fn test_parse_mti_from_request_should_be_success() {
         let fields = vec![Field {
-            id: "2".to_string(),
-            value: "5276600404324025".to_string(),
+            id: "0".to_string(),
+            value: "0100".to_string(),
         }];
 
         let request = ISORequestMessage { fields };
 
-        let iso = ISOMessage::from(request);
+        let iso = ISOMessage::from(&request);
+        assert_eq!(iso.is_some(), true);
 
-        assert_eq!(iso.mti, MessageTypeIndicator::AuthorizationRequest);
+        let unwrap_iso = iso.unwrap();
+        assert_eq!(unwrap_iso.mti, MessageTypeIndicator::AuthorizationRequest);
+    }
+
+    #[test]
+    fn test_parse_mti_from_request_should_be_invalid() {
+        let fields = vec![Field {
+            id: "1".to_string(),
+            value: "0100".to_string(),
+        }];
+
+        let request = ISORequestMessage { fields };
+
+        let iso = ISOMessage::from(&request);
+
+        assert_eq!(iso.is_none(), true);
     }
 }
