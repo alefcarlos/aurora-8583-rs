d01fe5d360ef12f319a73a58e0514476c5ea2404
Alef Carlos <alef.carlos@gmail.com>
2020-04-17 00:04:28 -0300

    fix: example

--- src/main.rs
+++ src/main.rs
@@ -6,40 +6,6 @@ use requests::*;
 use std::convert::TryFrom;
 
 fn main() {
-    //      request example
-    //     <isomsg direction="incoming">
-    //     <!-- org.jpos.iso.packager.GenericPackager[MCPackager.xml] -->
-    //     <field id="0" value="0100"/>
-    //     <field id="2" value="5276600404324025"/>
-    //     <field id="3" value="000000"/>
-    //     <field id="4" value="000000008500"/>
-    //     <field id="6" value="000000016609"/>
-    //     <field id="7" value="0318135214"/>
-    //     <field id="10" value="61954000"/>
-    //     <field id="11" value="000003"/>
-    //     <field id="12" value="135214"/>
-    //     <field id="13" value="0318"/>
-    //     <field id="14" value="2412"/>
-    //     <field id="15" value="0318"/>
-    //     <field id="16" value="0318"/>
-    //     <field id="18" value="5999"/>
-    //     <field id="22" value="051"/>
-    //     <field id="23" value="000"/>
-    //     <field id="32" value="009685"/>
-    //     <field id="35" value="5276600404324025D24122060000024600"/>
-    //     <field id="37" value="123456789012"/>
-    //     <field id="41" value="Reg00004"/>
-    //     <field id="42" value="687555537877464"/>
-    //     <field id="43" value="Midwest Emporium      Columbia     MO "/>
-    //     <field id="48" value="52383030325456" type="binary"/>
-    //     <field id="49" value="840"/>
-    //     <field id="51" value="986"/>
-    //     <field id="52" value="4F5B4469CF4C4900" type="binary"/>
-    //     <field id="53" value="9701010003000000"/>
-    //     <field id="55" value="5F2A020840820258008407A0000000041010950500000000009A032003189C01009F02060000000085009F10120114250000044000DAC100000000000000009F1A0208409F260850890DB8B188364C9F2701809F3303E0E8E89F34034103029F360200049F370422B4C72A" type="binary"/>
-    //     <field id="61" value="000000000050084090210"/>
-    //     <field id="63" value="MRG01121L"/>
-    //   </isomsg>
 
     println!("Hello, world!");
 
@@ -51,6 +17,14 @@ fn main() {
         Field {
             id: "2".to_string(),
             value: "5276600404324025".to_string(),
+        },
+        Field {
+            id: "3".to_string(),
+            value: "000000".to_string(),
+        },
+        Field {
+            id: "22".to_string(),
+            value: "051".to_string(),
         },
     ];
 
