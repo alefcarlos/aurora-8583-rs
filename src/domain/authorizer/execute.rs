be87b416b0fed5278ea50b4405017fb264ba95b0
Alef Carlos <alef.carlos@gmail.com>
2020-04-17 22:00:17 -0300

    todos:

--- src/domain/authorizer/execute.rs
+++ src/domain/authorizer/execute.rs
@@ -13,13 +13,21 @@ pub enum AuthorizerResult {
 }
 
 pub fn execute_auth_flow(trx: TransactionType) -> Result<AuthorizerResult, AuthorizerError> {
+    //TODO: persistir transacao no banco
+
+    //TODO: emitir evento de transacao recebida
+
     let result = match trx {
         TransactionType::OlinePurchase(message) => execute_online_purchase(&message),
         _ => Err(AuthorizerError::InvalidTransaction)
     };
 
-    match result {
+    let response  = match result {
         Ok(v) => Ok(v),
         Err(v) => Err(v)
-    }
+    };
+
+    //TODO: emitir evento de transacao negada/aprovada
+
+    response
 }
